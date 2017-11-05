f13bdbf69cd41c6cc5e33086aa0569dceda8fcf1
artek744 <ablaszczyk44@gmail.com>
2017-11-05 12:50:23 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -7,23 +7,23 @@ NC="\033[0m"
 function install_dependencies {
 while true
 do
-	read -r -p 'Do you want to install dependencies? [y/n] ' choice
-	case "$choice" in
-		n|N) break;;
-		y|Y)	echo -e $CYAN "\nUpdating repositories ..." $NC
-				sudo apt-get update
-				echo -e $CYAN "\nInstalling dependencies ..." $NC
-				sudo apt-get install vim-gtk vim-gnome exuberant-ctags
-	  		  break;;
-		*) echo 'Response not valid';;
-	esac
+   read -r -p 'Do you want to install dependencies? [y/n] ' choice
+   case "$choice" in
+      n|N) break;;
+      y|Y)	echo -e $CYAN "\nUpdating repositories ..." $NC
+            sudo apt-get update
+            echo -e $CYAN "\nInstalling dependencies ..." $NC
+            sudo apt-get install vim-gtk vim-gnome exuberant-ctags
+           break;;
+      *) echo 'Response not valid';;
+   esac
 done
 }
 
 cp -r .vim ~/
 cp -r .vimrc ~/
 
-install_vim_dependencies
+install_dependencies
 
 echo -e $GREEN "\nVim config files have been instaled.\n" $NC
 
