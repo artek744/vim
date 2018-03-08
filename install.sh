47c668398735c1a0a46b34eef79459c0fabfce85
artek744 <ablaszczyk44@gmail.com>
2018-03-08 21:43:22 +0100

    Update install script

--- install.sh
+++ install.sh
@@ -1,30 +1,52 @@
 #!/bin/bash
 
 CYAN="\033[0;96m"
-GREEN="\033[1;32m"
-NC="\033[0m"
+CYAN_BOLD="\033[1;96m"
+GREEN="\033[0;32m"
+GREEN_BOLD="\033[1;32m"
+YELLOW="\033[0;33m"
+YELLOW_BOLD="\033[1;33m"
+RESET="\033[0m"
 
-function install_dependencies {
-while true
-do
-   read -r -p 'Do you want to install dependencies? [y/n] ' choice
-   case "$choice" in
-      n|N) break;;
-      y|Y)	echo -e $CYAN "\nUpdating repositories ..." $NC
-            sudo apt-get update
-            echo -e $CYAN "\nInstalling dependencies ..." $NC
-            sudo apt-get install vim-gtk vim-gnome exuberant-ctags
-           break;;
-      *) echo 'Response not valid';;
-   esac
-done
+function copy_configs
+{
+	echo -e $CYAN "\nCopying configs ..." $RESET
+
+	cp -rf .vim ~/
+	cp -rf .vimrc ~/
 }
 
-cp -r .vim ~/
-cp -r .vimrc ~/
+function install_submodules
+{
+	echo -e $CYAN "\nInstalling submodules ..." $RESET
+	git submodule update --init --recursive
+}
 
+function install_dependencies
+{
+	dependencies="vim vim-gtk vim-gnome exuberant-ctags"
+	echo -e "\nDependencies: "$YELLOW_BOLD $dependencies $RESET
+
+	while true
+	do
+		read -p "$(echo -e "Do you want to install dependencies? [y/n]: "$RESET)" choice
+		case "$choice" in
+			n|N) break;;
+			y|Y)	echo -e $CYAN "\nUpdating repositories ..." $RESET
+					sudo apt-get update
+					echo -e $CYAN "\nInstalling dependencies ..." $RESET
+					sudo apt-get install $dependencies
+				  break;;
+			*) echo -e '\nIncorrect response, please type '\''y'\'' or '\''n'\';;
+		esac
+	done
+}
+
+
+copy_configs
 install_dependencies
+install_submodules
 
-echo -e $GREEN "\nVim config files have been instaled.\n" $NC
+echo -e $GREEN_BOLD "\nVim config files have been instaled.\n" $RESET
 
 
