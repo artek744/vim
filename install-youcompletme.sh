28c9786617e917e150ccfd82bb86a0dfea3b3939
artek744 <ablaszczyk44@gmail.com>
2018-09-07 16:22:16 +0200

    Update youcompletme install script

--- install-youcompletme.sh
+++ install-youcompletme.sh
@@ -1,5 +1,6 @@
 main()
 {
+	rm -fr .vim/bundle/youcompleteme
 	git clone https://github.com/Valloric/YouCompleteMe .vim/bundle/youcompleteme --recursive
 	cd .vim/bundle/youcompleteme
 	./install.py --clang-completer --system-libclang
